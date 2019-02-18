4739b5bfeb3bbf0367cf7d514056391220ef77ae
Shawn.J.Tseng (tw mis.twtc01.Newegg) 61878 <st7j@buyabs.corp>
2019-02-18 16:25:31 +0800

    屬性綁定(attribute bind)

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ var app = new Vue({
         message: 'Hello World~'
     }
 })
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: '現在時間是 ' + new Date().toLocaleString()
+    }
+})
