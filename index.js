a54bf836825aa3e0fe8fa7f7654dd5c3d85bfe53
Shawn.J.Tseng (tw mis.twtc01.Newegg) 61878 <st7j@buyabs.corp>
2019-02-18 16:29:42 +0800

    結構型指令(structual directive) - if, for

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ var app2 = new Vue({
         message: '現在時間是 ' + new Date().toLocaleString()
     }
 })
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app4',
+    data: {
+        todos: [
+            {text: 'w3c javascript全部讀過'},
+            {text: 'postman導入'},
+            {text: 'IM部門的前端需求'}
+        ]
+    }
+})
