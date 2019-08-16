3c05951a5d774154456c3dd385e257be665988d9
ShawnTseng <shawntseng40@gmail.com>
2019-08-16 13:44:24 +0800

    屬性傳入(父組件=>子組件)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 Vue.component('list-item', {
-    template: '<li>第N個項目</li>'
+    props:['item'],
+    template: '<li>{{item.text}}</li>'
 })
 
 var app = new Vue({
@@ -9,9 +10,9 @@ var app = new Vue({
         loadTime: '網頁啟動於:' + new Date().toLocaleString(),
         showHint: true,
         dataList: [
-            { text: '第一個項目' },
-            { text: '第二個項目' },
-            { text: '第三個項目' }
+            { text: 'A項目' },
+            { text: 'B項目' },
+            { text: 'C項目' }
         ]
     },
     methods: {
