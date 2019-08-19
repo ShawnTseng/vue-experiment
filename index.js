1e9156570a29c418b16e193312a1ecdee1f9606d
ShawnTseng <shawntseng40@gmail.com>
2019-08-19 13:58:32 +0800

    新增 created hook

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 Vue.component('list-item', {
-    props:['item'],
+    props: ['item'],
     template: '<li>{{item.text}}</li>'
 })
 
+var data = {
+    title: 'Hello World',
+    loadTime: '網頁啟動於:' + new Date().toLocaleString(),
+    showHint: true,
+    dataList: [
+        { text: 'A項目' },
+        { text: 'B項目' },
+        { text: 'C項目' }
+    ]
+}
+
+var crated = function () {
+    console.log('this', this);
+}
+
 var app = new Vue({
     el: '#app',
-    data: {
-        title: 'Hello World',
-        loadTime: '網頁啟動於:' + new Date().toLocaleString(),
-        showHint: true,
-        dataList: [
-            { text: 'A項目' },
-            { text: 'B項目' },
-            { text: 'C項目' }
-        ]
-    },
+    data: data,
+    created: crated,
     methods: {
         switchShowHint: function () {
             this.showHint = !this.showHint
