ab10851ce667321a9612c654201bdf62ad32eb77
ShawnTseng <shawntseng40@gmail.com>
2019-09-26 13:46:17 +0800

    子組件事件監聽 (含參數傳遞)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ Vue.component('my-header', {
     props: ['title'],
     template: `<div>
     <h1>{{title}}</h1>
-    <button v-on:click="$emit('event-emit-test')">event emit</button>
+    <button v-on:click="$emit('event-emit-test',2)">event emit</button>
 </div>
     `
 })
